0749df0b61e8afa6635d5e76cbb90472cd912dd5
Rafael Rosa <rafaelsahyoun@gmail.com>
2023-01-26 19:47:52 -0300

    1commit

---
+++ src/main/java/com/rafaelrosa/springboot3mongoDBmoduleworkshop/Springboot3MongoDbModuleWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.rafaelrosa.springboot3mongoDBmoduleworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3MongoDbModuleWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot3MongoDbModuleWorkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafaelrosa/springboot3mongoDBmoduleworkshop/Springboot3MongoDbModuleWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelrosa.springboot3mongoDBmoduleworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3MongoDbModuleWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
